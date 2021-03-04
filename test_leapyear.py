5b51946e295ea7146fa3859647a79591390d36f9
Karina Munoz <munozka@oregonstate.edu>
2021-03-04 15:07:47 -0800

    second test, ValueError

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,7 +4,8 @@ import leapyear
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.leapyear(2000), "{0} is a leap year".format(2000))
-
+    def test2(self):
+        self.assertRaises(ValueError)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)    
